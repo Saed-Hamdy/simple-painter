8283458e1828e463c43cffcdfe15bfc353f14bfc
Ahmed Yakout <iyakout@hotmail.com>
2016-11-03 15:57:03 +0200

    remove redundent src dir from jnlp branch

--- src/shapes/Square.java
+++
@@ -1,8 +0,0 @@
-package shapes;
-
-public class Square extends Rectangle {
-
-    public Square(Point location,Dimensions dimensions) {
-        super(location, new Dimensions(Math.max(dimensions.height, dimensions.width), Math.max(dimensions.height, dimensions.width)));
-    }
-}
